e260e243477d12be839d1d9c65af829efc50c0d1
volodyan <mail.volodymyr@gmail.com>
2018-02-21 11:14:17 +0200

    Some changes made

--- corporate-blue/js/main.js
+++ corporate-blue/js/main.js
@@ -1,14 +1,14 @@
-$(function() {
-    var pull        = $('#pull'),
-        menu        = $('.main-menu > ul');
+var pull        = $('#pull'),
+    menu        = $('.main-menu > ul');
 
+$(function() {
     $(pull).on('click', function(e) {
         e.preventDefault();
         menu.slideToggle();
     });
 });
 
-$(window).resize(function(){
+$(window).resize(function() {
     var w = $(window).width();
     if(w > 420 && menu.is(':hidden')) {
         menu.removeAttr('style');
